ac7d579e226b5b559a8899f6af8f970b8feff936
. <lazizbekabdullayev118@gmail.com>
2025-04-06 20:44:26 +0500

    dsadas

--- server/db.js
+++ server/db.js
@@ -3,8 +3,8 @@ const { exit } = require("process");
 
 const connect = async () => {
   await mongoose
-    // .connect("mongodb+srv://lazizbekabdullayev118:8mJhRMX9C2BHgOnQ@cluster0.yu5xyhp.mongodb.net/stoploto?retryWrites=true&w=majority&appName=Cluster0")
-    .connect("mongodb://localhost:27017/telegram")
+    .connect("mongodb+srv://lazizbekabdullayev118:YlOwwhF11hdRtW64@portfolioc.7pmq5y0.mongodb.net/?retryWrites=true&w=majority&appName=PortfolioC")
+    // .connect("mongodb://localhost:27017/telegram")
     .then(() => {
       console.log("\u001b[32;1m ⚙️  App has connected to MongoDB");
     })
